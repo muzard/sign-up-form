8aef5d44dc3e5dce62e26eb24bf80fbd4853990a
muzard <arttu.kokki@gmail.com>
2022-12-12 19:03:40 +0200

    enhance script

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function checkIfPasswordsMatch() {
-    pw = document.getElementById("pwd").value
-    c_pw = document.getElementById("c-pwd").value
+  let pw = document.getElementById("pwd");
+  let c_pw = document.getElementById("c-pwd");
 
-    pwd_text = document.getElementById("pwd-text").textContent
+  let pwd_text = document.getElementById("pwd-text");
 
-    if (pw !== pwd) {
-        pwd_text = "Passwords need to match!"
-    }
+  if (pw.value !== c_pw.value) {
+    pwd_text.textContent = "Passwords need to match!";
+
+    pw.classList.add("invalid")
+    c_pw.classList.add("invalid")
+  } else if (pw.value == c_pw.value) {
+    pwd_text.textContent = null
+
+    pw.classList.remove("invalid")
+    c_pw.classList.remove("invalid")
+  }
 }
